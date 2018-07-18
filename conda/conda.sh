f62011a4e89e42c8485223ee0a0dca7f16d56c57
tritest <36116920+tritest@users.noreply.github.com>
2018-07-19 01:13:28 +0200

    Update conda.sh

--- conda/conda.sh
+++ conda/conda.sh
@@ -17,7 +17,7 @@ rm -rf $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin_tmp
 ARCH=x86_64 $TRAVIS_BUILD_DIR/appimagetool-x86_64.AppImage $TRAVIS_BUILD_DIR/conda/AppDir
 
 #4 setting rights for the appimage
-chmod +x $TRAVIS_BUILD_DIR/conda/FreeCAD-x86_64.AppImage
+chmod +x *.AppImage
 
 #5 delete the created environment
 rm -rf $TRAVIS_BUILD_DIR/conda/AppDir/usr
