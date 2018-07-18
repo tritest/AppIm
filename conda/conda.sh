913157d6154cd35f2e6fbb6019170ccb1090507c
tritest <36116920+tritest@users.noreply.github.com>
2018-07-19 00:37:46 +0200

    Update conda.sh

--- conda/conda.sh
+++ conda/conda.sh
@@ -1,25 +1,25 @@
 #1 create a new environment in the AppDir with packages specified with a text-file
-conda create -p AppDir/usr --file freecad-spec-file.txt --copy
+conda create -p $TRAVIS_BUILD_DIR/conda/AppDir/usr --file freecad-spec-file.txt --copy
 
 #2 delete unnecessary stuff
-rm -rf AppDir/usr/include
-find AppDir/usr -name \*.a -delete
-mv AppDir/usr/bin AppDir/usr/bin_tmp
-mkdir AppDir/usr/bin
-cp AppDir/usr/bin_tmp/FreeCAD AppDir/usr/bin/FreeCAD
-cp AppDir/usr/bin_tmp/activate AppDir/usr/bin/  
-cp AppDir/usr/bin_tmp/python AppDir/usr/bin/
-cp AppDir/usr/bin_tmp/widget.py AppDir/usr/bin/
-rm -rf AppDir/usr/bin_tmp
+rm -rf $TRAVIS_BUILD_DIR/conda/AppDir/usr/include
+find $TRAVIS_BUILD_DIR/conda/AppDir/usr -name \*.a -delete
+mv $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin_tmp
+mkdir $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin
+cp $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin_tmp/FreeCAD $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin/FreeCAD
+cp $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin_tmp/activate $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin/  
+cp $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin_tmp/python $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin/
+cp $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin_tmp/widget.py $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin/
+rm -rf $TRAVIS_BUILD_DIR/conda/AppDir/usr/bin_tmp
 #+ deleting some specific libraries not needed. eg.: stdc++
 
 #3 create the appimage
-ARCH=x86_64 ../appimagetool-x86_64.AppImage AppDir
+ARCH=x86_64 $TRAVIS_BUILD_DIR/appimagetool-x86_64.AppImage $TRAVIS_BUILD_DIR/conda/AppDir
 
 #4 setting rights for the appimage
-chmod +x FreeCAD-x86_64.AppImage
+chmod +x $TRAVIS_BUILD_DIR/conda/FreeCAD-x86_64.AppImage
 
 #5 delete the created environment
-rm -rf AppDir/usr
+rm -rf $TRAVIS_BUILD_DIR/conda/AppDir/usr
 
 exit(0)
