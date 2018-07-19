281358a606f6cde654f17aee5923df7f3873e2d0
tritest <36116920+tritest@users.noreply.github.com>
2018-07-19 12:56:52 +0200

    Update version.py

--- conda/version.py
+++ conda/version.py
@@ -2,7 +2,6 @@ import os, glob
 path = os.path.dirname(__file__)
 os.chdir(path)
 f = glob.glob("*.AppImage")[0]
-v = FreeCAD.Version()
-v = "FreeCAD_" + v[0] + "." + v[1] + "." + v[2].split()[0] + "." + "_Py3Qt5_glibc2.12-x86_64.AppImage"
+v = "FreeCAD_0.18_Conda_Py3Qt5_glibc2.12-x86_64.AppImage"
 os.rename(os.path.join(path, f), os.path.join(path, v))
 exit(0)
