eea087099625e225dee3637fc44518d0bbdd6c26
tritest <36116920+tritest@users.noreply.github.com>
2018-07-20 10:26:47 +0200

    Update version.py

--- out/version.py
+++ out/version.py
@@ -3,6 +3,6 @@ path = os.path.dirname(__file__)
 os.chdir(path)
 f = glob.glob("*.AppImage")[0]
 v = FreeCAD.Version()
-v = "FreeCAD_" + v[0] + "." + v[1] + "." + v[2].split()[0] + "." + "_Py2Qt4_glibc2.17-x86_64.AppImage"
+v = "FreeCAD_" + v[0] + "." + v[1] + "." + v[2].split()[0] + "." + "glibc2.17-x86_64.AppImage"
 os.rename(os.path.join(path, f), os.path.join(path, v))
 exit(0)
