238154f4b0061766d76b7ccfe83c185233da1931
tritest <36116920+tritest@users.noreply.github.com>
2018-07-20 10:20:20 +0200

    Delete version.py

--- conda/version.py
+++
@@ -1,7 +0,0 @@
-import os, glob
-path = os.path.dirname(__file__)
-os.chdir(path)
-f = glob.glob("*.AppImage")[0]
-v = "FreeCAD_0.18_Conda_Py3Qt5_glibc2.12-x86_64.AppImage"
-os.rename(os.path.join(path, f), os.path.join(path, v))
-exit(0)
